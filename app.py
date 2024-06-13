2ded0d542baaee49c7a6d5d4e7474c78f06ef586
JesusFong23 <132445660+JesusFong23@users.noreply.github.com>
2024-06-13 01:49:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def login():
 # Load the dataset
 @st.cache_data  # Cache the dataset for better performance
 def load_data():
-    return pd.read_csv("Pokemon_with_images.csv")
+    return pd.read_csv("/Users/jesusfong/itd2024/data/raw/Pokemon_with_images.csv")
 
 # Pikachu mini-game page
 def pikachu_game():
@@ -156,7 +156,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
 
